6ef9f8cd106acb2b528d986dbf551946324ad0e5
hansvanatten <hansvanatten@gmail.com>
2017-02-18 13:47:33 +0100

    cleanup GamePage

---
+++ src/games/GameItem.test.js
@@ -0,0 +1,8 @@
+// src/recipes/RecipeItem.test.js
+import React from 'react'
+import chai, { expect } from 'chai'
+import { shallow } from 'enzyme'
+import chaiEnzyme from 'chai-enzyme'
+import { GameItem } from './GameItem'
+
+chai.use(chaiEnzyme)
---
+++ src/games/GamesContainer.test.js
@@ -0,0 +1,10 @@
+// src/recipes/GamesContainer.test.js
+import React from 'react'
+import chai, { expect } from 'chai'
+import { shallow } from 'enzyme'
+import chaiEnzyme from 'chai-enzyme'
+import { GamesContainer } from './GamesContainer'
+import Title from '../components/Title'
+import GameItem from './GameItem'
+
+chai.use(chaiEnzyme)
